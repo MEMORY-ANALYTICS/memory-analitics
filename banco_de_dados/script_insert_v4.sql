7b5a5f5e08c969882bd3ab55e5e9ae37fc2953d9
AlexandraHY14 <alexandra.yara@sptech.school>
2023-10-08 23:11:52 -0300

    Ajustando inserts v4, não finalizado

--- banco_de_dados/script_insert_v4.sql
+++ banco_de_dados/script_insert_v4.sql
@@ -7,6 +7,11 @@ insert into empresa values
 (null, 'Uceae', '01235794000012', 'uceae.contatar@gmail.com', '(11)91234-5763'),
 (null, 'Memory Analytics', '32564433000012', 'memory_analytics@gmail.com', '(11)97763-5425');
 
+-- Tabela Endereço
+insert into endereco values
+(null, '05425000', 'Rua Eugênio de Medeiros', 305, 'São Paulo', 'SP', '11º andar', 1),
+(null, '04538-133', 'Av. Brig. Faria Lima', '3477', 'São Paulo', 'SP', null, 2);
+
 -- Tabela Cargo
 insert into cargo values
 (null, 'Analista de Hardware'),
@@ -16,12 +21,12 @@ insert into cargo values
 
 -- Tabela Funcionário
 insert into funcionario values
-(null, 'João Miguel Almeida', 'joao@gmail.com','11983220192', 1, 3, null),
-(null, 'Marcos Araújo', 'marcos@gmail.com','11933340099', 1, 2, 1),
-(null, 'Solange Medeiros', 'solange@gmail.com','11983228776', 1, 1, 1),
-(null, 'Luan Santos', 'luan@gmail.com','11923280192', 2, 3, null),
-(null, 'Maria da Penha', 'maria@gmail.com','11983522190', 2, 2, 4),
-(null, 'Larissa Pinheiro de Brito', 'larissa@gmail.com','11912230112', 2, 1, 4);
+(null, 'João Miguel Almeida', 'joao@gmail.com','11983220192', 1, 1, 3, null),
+(null, 'Marcos Araújo', 'marcos@gmail.com','11933340099', 2, 1, 1, 1),
+(null, 'Solange Medeiros', 'solange@gmail.com','11983228776', 3, 1, 2, 1),
+(null, 'Luan Santos', 'luan@gmail.com','11923280192', 1, 2, 3, null),
+(null, 'Maria da Penha', 'maria@gmail.com','11983522190', 2, 2, 2, 4),
+(null, 'Larissa Pinheiro de Brito', 'larissa@gmail.com','11912230112', 2, 2, 1, 4);
 
 -- Tabela Login
 insert into login values
@@ -48,14 +53,7 @@ insert into servidores values
 -- (null, 'CPU', 'CPU 1'),
 -- (null, 'REDE', null);
 
--- Tabela Nome Componente
--- rever atributos dessa tabela
-insert into nomeComponente values
-(null, 'Disco'),
-(null, 'Memória RAM'),
-(null, 'CPU'),
-(null, 'Rede');
-
+-- Tabela Medida Componente
 insert into medidaComponente values
 (null, 'Temperatura', '°C', 'Celsius'),
 (null, 'Uso', '%', 'Porcentagem'),
@@ -65,17 +63,34 @@ insert into medidaComponente values
 (null, 'Unidade', null, null),
 (null, 'Tempo', 's', 'Segundos'),
 (null, 'Frequencia', 'GHz', 'Giga Hertz');
-   
--- insert into MedidaComponente values
--- (null, 5 , 1 , 2),
--- (null, 5 , 2 , 2),
--- (null, 5 , 3 , 2),
--- (null, 5 , 4 , 6);
+
+-- Tabela Métrica Componente
+insert into metricaComponente values
+(null, 20, 25); -- Graus Celsius
+
+-- Tabela Nome Componente
+-- rever atributos dessa tabela
+insert into nomeComponente values
+(null, 'Disco'),
+(null, 'Memória RAM'),
+(null, 'CPU'),
+(null, 'Placa de Rede');
+
+-- Tabela Modelo Componente
+insert into modeloComponente values
+(null, 'Intel', 'Xeon E5-2697 V2', '?'),
+(null, 'AMD', 'Ryzen 9 5900X', '?'),
+(null, 'Dell', '32Go - 2RX4 DDR4 RDIMM', '?');
+-- Onde ver o número da geração dos modelos
+-- placa de rede fabricantes e modelos?
 
 -- Tabela SubComponente
 -- insert into subComponente values
 -- (),
 
+-- Tabela Componente Completo
+
+-- Tabela Componente
 
 -- Tabela Registro
 insert into registro values
