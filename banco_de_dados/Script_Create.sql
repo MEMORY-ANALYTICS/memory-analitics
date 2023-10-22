c20be012365091933a1b038fe6673f7e9676336b
RaphaelKlein <raphael.almeida@sptech.school>
2023-10-22 18:06:59 -0300

    SCRIPT NOVO BASEADO NA MODELAGEM NOVA

--- banco_de_dados/Script_Create.sql
+++ banco_de_dados/Script_Create.sql
@@ -62,171 +62,36 @@ fkEmpresa INT,
 FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
-CREATE TABLE IF NOT EXISTS `tipoComponente`(
-idTipoComponente INT PRIMARY KEY AUTO_INCREMENT,
-tipoComponente VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS `modeloComponente`(
-idModeloComponente INT PRIMARY KEY AUTO_INCREMENT,
-fabricante VARCHAR(45),
-nomeModelo VARCHAR(45),
-codigogeracao VARCHAR(45),
-fkTipoComponente INT,
-FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente (idTipoComponente)
-);
-
-CREATE TABLE IF NOT EXISTS `metricaComponente`(
-idMetricaComponente INT PRIMARY KEY AUTO_INCREMENT,
-limiteMin VARCHAR(45),
-limiteMax VARCHAR(45),
-fkModeloComponente INT,
-FOREIGN KEY (fkModeloComponente) REFERENCES modeloComponente (idModeloComponente)
-);
-
 CREATE TABLE IF NOT EXISTS `componente`(
 idComponente INT PRIMARY KEY AUTO_INCREMENT,
+fabricante VARCHAR(45),
+nomeModelo VARCHAR(45),
+tipoComponente VARCHAR(45),
+limiteMin VARCHAR(45),
+limiteMax VARCHAR(45),
 fkServidor INT,
-fkModeloComponente INT,
-FOREIGN KEY (fkServidor) REFERENCES servidor (idServidor),
-FOREIGN KEY (fkModeloComponente) REFERENCES modeloComponente (idModeloComponente)
+FOREIGN KEY (fkServidor) REFERENCES servidor (idServidor)
 );
 
-CREATE TABLE IF NOT EXISTS `subComponente`(
-idSubComponente INT PRIMARY KEY AUTO_INCREMENT,
-nomeSubComponente VARCHAR(45),
-fkComponente INT,
-FOREIGN KEY (fkComponente) REFERENCES componente (idComponente)
+CREATE TABLE IF NOT EXISTS `recurso`(
+  idRecurso INT PRIMARY KEY AUTO_INCREMENT,
+  tipoRecurso VARCHAR(45),
+  fkComponente INT,
+  FOREIGN KEY (fkComponente) REFERENCES componente (idComponente)
 );
 
 CREATE TABLE IF NOT EXISTS `medidaComponente`(
-idMedidaComponente INT PRIMARY KEY AUTO_INCREMENT,
-nomeMedida VARCHAR(45),
-simboloMedida VARCHAR(10),
-unidadeMedida VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS `detalheSubComponente`(
-fkSubComponente INT NOT NULL,
-fkMedidaComponente INT NOT NULL,
-FOREIGN KEY (fkSubComponente) REFERENCES subComponente (idSubComponente),
-FOREIGN KEY (fkMedidaComponente) REFERENCES medidaComponente (idMedidaComponente),
-PRIMARY KEY (fkSubComponente, fkMedidaComponente)
+  idMedidaComponente INT,
+  tipoMedida VARCHAR(25),
+  unidadeMedida VARCHAR(45)
 );
 
 CREATE TABLE IF NOT EXISTS `registro`(
-idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-valorRegistro DOUBLE,
-dtHoraRegistro DATETIME,
-fkDSCSubComponente INT,
-fkDSCMedidaComponente INT,
-FOREIGN KEY (fkDSCSubComponente) REFERENCES detalheSubComponente (fkSubComponente),
-FOREIGN KEY (fkDSCMedidaComponente) REFERENCES detalheSubComponente (fkMedidaComponente)
+  idRegistro INT,
+  valorRegistro DOUBLE,
+  dtHoraRegistro DATETIME,
+  fkRecurso INT,
+  fkMedidaComponente INT,
+  FOREIGN KEY (fkRecurso) REFERENCES recurso (idRecurso),
+  FOREIGN KEY (fkMedidaComponente) REFERENCES medidaComponente (idMedidaComponente)
 );
-
--- Inserir dados na tabela 'empresa'
-INSERT INTO empresa (nomeEmpresa, cnpjEmpresa, emailEmpresa, telEmpresa) VALUES
-('Empresa A', '12345678901234', 'empresaA@email.com', '1122334455'),
-('Empresa B', '56789012345678', 'empresaB@email.com', '2233445566'),
-('Empresa C', '90123456789012', 'empresaC@email.com', '3344556677');
-
--- Inserir dados na tabela 'endereco'
-INSERT INTO endereco (cep, logradouro, numero, cidade, estado, fkEmpresa) VALUES
-('12345678', 'Rua A', 123, 'Cidade A', 'Estado A', 10000),
-('23456789', 'Rua B', 456, 'Cidade B', 'Estado B', 10001),
-('34567890', 'Rua C', 789, 'Cidade C', 'Estado C', 10002);
-
--- Inserir dados na tabela 'cargo'
-INSERT INTO cargo (nomeCargo) VALUES
-('Gerente'),
-('Analista'),
-('Técnico');
-
--- Inserir dados na tabela 'funcionario'
-INSERT INTO funcionario (nomeFunc, emailFunc, telefoneFunc, permissao, fkEmpresa, fkCargo, fkSupervisor) VALUES
-('João', 'joao@email.com', '11122233344', 'A', 10000, 1, NULL),
-('Maria', 'maria@email.com', '22233344455', 'A', 10001, 2, 100000),
-('Pedro', 'pedro@email.com', '33344455566', 'B', 10002, 3, 100000);
-
--- Inserir dados na tabela 'login'
-INSERT INTO login (login, senha, fkFuncionario) VALUES
-('joao123', 'senha123', 100000),
-('maria456', 'senha456', 100001),
-('pedro789', 'senha789', 100002);
-
--- Inserir dados na tabela 'servidor'
-INSERT INTO servidor (SistemaOperacionalServidor, apelidoServidor, ipServidor, numeroSerieServidor, fkEmpresa) VALUES
-('Linux', 'Servidor A', '192.168.1.1', 'SERV123', 10000),
-('Windows', 'Servidor B', '192.168.1.2', 'SERV456', 10001),
-('Linux', 'Servidor C', '192.168.1.3', 'SERV789', 10002);
-
--- Inserir dados na tabela 'tipoComponente'
-INSERT INTO tipoComponente (tipoComponente) VALUES
-('Sensor'),
-('Atuador'),
-('Controlador');
-
--- Inserir dados na tabela 'modeloComponente'
-INSERT INTO modeloComponente (fabricante, nomeModelo, codigogeracao, fkTipoComponente) VALUES
-('Fabricante A', 'Modelo X', 'COD123', 1),
-('Fabricante B', 'Modelo Y', 'COD456', 2),
-('Fabricante C', 'Modelo Z', 'COD789', 3);
-
--- Inserir dados na tabela 'metricaComponente'
-INSERT INTO metricaComponente (limiteMin, limiteMax, fkModeloComponente) VALUES
-('0', '100', 1),
-('10', '90', 2),
-('20', '80', 3);
-
--- Inserir dados na tabela 'componente'
-INSERT INTO componente (fkServidor, fkModeloComponente) VALUES
-(1, 1),
-(2, 2),
-(3, 3);
-
--- Inserir dados na tabela 'subComponente'
-INSERT INTO subComponente (nomeSubComponente, fkComponente) VALUES
-('SubComponente 1', 1),
-('SubComponente 2', 2),
-('SubComponente 3', 3);
-
--- Inserir dados na tabela 'medidaComponente'
-INSERT INTO medidaComponente (nomeMedida, simboloMedida, unidadeMedida) VALUES
-('Temperatura', '°C', 'Celsius'),
-('Pressão', 'Pa', 'Pascal'),
-('Umidade', '%', 'Porcentagem');
-
-INSERT INTO detalheSubComponente VALUES
-(1, 1),
-(2, 2),
-(3, 3);
-
--- Inserir dados na tabela 'registro'
-INSERT INTO registro (idRegistro, valorRegistro, dtHoraRegistro, fkDSCSubComponente, fkDSCMedidaComponente) VALUES
-(null, 25.5, '2023-10-09 10:00:00', 1, 1), #Seguros
-(null, 31.5, '2023-10-09 10:30:00', 1, 1),
-(null, 50.0, '2023-10-09 10:45:00', 1, 1),
-(null, 35.0, '2023-10-09 11:00:00', 2, 2),
-(null, 28.0, '2023-10-09 11:30:00', 2, 2),
-(null, 61.0, '2023-10-09 11:45:00', 2, 2),
-(null, 50.0, '2023-10-09 12:00:00', 3, 3),
-(null, 62.0, '2023-10-09 12:30:00', 3, 3),
-(null, 44.5, '2023-10-09 12:45:00', 3, 3),
-(null, 90.0, '2023-10-09 13:00:00', 1, 1), #Alertas
-(null, 95.0, '2023-10-09 13:30:00', 1, 1),
-(null, 12.0, '2023-10-09 14:00:00', 2, 2),
-(null, 84.0, '2023-10-09 14:30:00', 2, 2),
-(null, 75.0, '2023-10-09 15:00:00', 3, 3),
-(null, 23.0, '2023-10-09 15:30:00', 3, 3),
-(null, -5.0, '2023-10-09 16:00:00', 1, 1), # Criticos
-(null, 109.5, '2023-10-09 16:30:00', 2, 2), 
-(null, 97.0, '2023-10-09 17:00:00', 3, 3);
-
-select * from empresa;
-select * from endereco;
-select * from funcionario;	
-select * from login;
-
-SELECT idFuncionario, nomeFunc, emailFunc, telefoneFunc, fkCargo, fkEmpresa FROM funcionario
-	join login on idFuncionario = fkFuncionario
-  WHERE email = '' AND senha = '';
