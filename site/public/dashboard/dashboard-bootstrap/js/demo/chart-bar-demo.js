dd86e160e3f10918159e9033c832040914936e20
danielandrade108 <daniel.andrade@sptech.school>
2023-10-24 09:42:59 -0300

    grafico bar

--- site/public/dashboard/dashboard-bootstrap/js/demo/chart-bar-demo.js
+++ site/public/dashboard/dashboard-bootstrap/js/demo/chart-bar-demo.js
@@ -49,11 +49,11 @@ var myBarChart = new Chart(ctx, {
   data: {
     labels: ["Maio", "Junho", "Julho", "Agosto", "Setembro"],
     datasets: [{
-      label: "Revenue",
+      label: "Quantidade",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [],
+      data: [7,6,5,5,5],
     }],
   },
   options: {
@@ -118,7 +118,7 @@ var myBarChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ':' + number_format(tooltipItem.yLabel);
         }
       }
     },
