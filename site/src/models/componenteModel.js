dbaa310baa2bc79529847d4e95d57a837099ee8e
gustavodessunte <gustavo.bernardes@sptech.school>
2023-10-27 12:56:06 -0300

    componenteModel.js -> Adicionada a rota do getAll de componentes

--- site/src/models/componenteModel.js
+++ site/src/models/componenteModel.js
@@ -13,6 +13,18 @@ function cadastrarComponente(fabricante, nomeModelo, tipoComponente, limiteMin, 
     return database.executar(instrucao);
 }
 
+function getAll(fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():");
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+    select nomeModelo, fabricante, tipoComponente, limiteMin, limiteMax, apelidoServidor from componente JOIN servidor on fkServidor = idServidor JOIN empresa on fkEmpresa = idEmpresa where fkEmpresa = ${fkEmpresa};`;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
-    cadastrarComponente
+    cadastrarComponente,
+    getAll
 } 
