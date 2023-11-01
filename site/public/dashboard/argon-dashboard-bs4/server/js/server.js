6ff732eeabe1ff7fdca0ab14869636983b79cea6
gustavodessunte <gustavo.bernardes@sptech.school>
2023-11-01 11:28:15 -0300

    cadastroServer.js -> removido; visualServer.js -> Removido; server.js -> Junção dos dois arquivos removidos; servers.html -> adicionada a modal de cadastro de servidor;

--- site/public/dashboard/argon-dashboard-bs4/server/js/server.js
+++ site/public/dashboard/argon-dashboard-bs4/server/js/server.js
@@ -29,7 +29,7 @@ function getAllServers(fkEmpresa) {
                   <i class="fas fa-ellipsis-v"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#exampleModalCenter" onclick="getInfosServidor(${resposta[0].idServidor})">
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#exampleModalCenter" onclick="getInfosServidor(${resposta[i].idServidor})">
                         Editar     
                   </a>
                   <a class="dropdown-item" href="#">Deletar</a>
@@ -122,3 +122,70 @@ function getInfosServidor(idServidor){
       console.error(`Erro na obtenção dos dados p/ Usuario: ${error.message}`);
     });
 }
+
+function cadastrarServidor() {
+  var erro = false;
+  var SistemaOperacionalServidor = sistema_operacional_adicionar.value;
+  var apelidoServidor = apelido_servidor_adicionar.value;
+  var ipServidor = ip_server_adicionar.value;
+  var numeroSerieServidor = num_serie_adicionar.value;
+  var fkEmpresa = sessionStorage.getItem("EMPRESA_USUARIO");
+
+  if(SistemaOperacionalServidor == ""){
+      
+      sistema_operacional.style ='border-color: red !important'
+      erro=true
+  }
+  if(apelidoServidor == ""){
+     
+      apelido_servidor.style ='border-color: red !important'
+      erro=true
+  }
+  if(ipServidor == ""){
+      
+      ip_server.style ='border-color: red !important'
+      erro=true
+  }
+  
+  if(numeroSerieServidor == ""){
+      
+      num_serie.style='border-color: red !important'
+      erro=true
+  } 
+  if(fkEmpresa == "" || undefined){ 
+      alert("fkEmpresa não pode ser nulo!")
+  }
+  if (!erro) {
+    fetch(`/servidor/cadastrar`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          SistemaOperacionalServidor:SistemaOperacionalServidor,
+          apelidoServidor:apelidoServidor,
+          ipServidor:ipServidor,
+          numeroSerieServidor:numeroSerieServidor,
+          fkEmpresa:fkEmpresa,
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+          console.log("Cadastrado com sucesso!");
+          document.getElementById("tableServers").innerHTML = "";
+          getAllServers(sessionStorage.getItem("EMPRESA_USUARIO"));
+        } else {
+          throw alert(
+            "Houve um erro ao tentar realizar o cadastro do Servidor!"
+          );
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
+  }else {
+    alert("Houve um erro ao tentar realizar o cadastro do Servidor!")
+  }
+} 
