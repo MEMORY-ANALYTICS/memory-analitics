8424cb5686f5af41cf9c62c6d437f066f06d9c3d
RaphaelKlein <raphael.almeida@sptech.school>
2023-11-14 12:15:41 -0300

    Melhorias no python

--- site/src/python/main.py
+++ site/src/python/main.py
@@ -1,16 +1,13 @@
 from captura_dados import *
 import datetime
-from gui import Gui
 def main(data_formatada):
     if(verificacao == True):
         osv = plat.uname().system
-        cpu = exibir_dados_cpu(data_formatada)
-        mem = exibir_info_mem(data_formatada)
-        disco = exibir_info_disco(data_formatada)
-        rede = exibir_info_rede(data_formatada)
-        temp = exibir_info_temp(osv,data_formatada)
-        # tela = Gui(osv, cpu, mem, disco, temp, rede)
-        # tela.mainloop()
+        exibir_dados_cpu(data_formatada)
+        exibir_info_mem(data_formatada)
+        exibir_info_disco(data_formatada)
+        exibir_info_rede(data_formatada)
+        exibir_info_temp(osv,data_formatada)
     else:
         print(f"Desculpe, o servidor {nome_user} ainda não foi cadastrado em nosso sistema, por favor acesse nosso site e cadastre este servidor.")
 if __name__ == "__main__":
