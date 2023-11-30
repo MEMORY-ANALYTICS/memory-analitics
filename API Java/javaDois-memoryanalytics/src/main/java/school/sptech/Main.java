0ec5fb2e2d8b788897e02b083af832b5e6b44765
gustavodessunte <gustavo.bernardes@sptech.school>
2023-11-29 21:00:24 -0300

    Processos.java -> Adicionado os métodos de get informações dos processos

--- API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Main.java
+++ API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Main.java
@@ -4,6 +4,7 @@ import com.github.britooo.looca.api.group.temperatura.Temperatura;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import school.sptech.BancoDados.*;
+import school.sptech.Recurso.Processos;
 import school.sptech.Servidores.Servidor;
 
 import java.util.List;
@@ -12,12 +13,28 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ConexaoMySql mysql = new ConexaoMySql();
-    JdbcTemplate con = mysql.criarConexao();
-    List<Servidor> l1 = con.query("select * from servidor",new BeanPropertyRowMapper<>(Servidor.class));
-   System.out.println(l1);
+//        ConexaoMySql mysql = new ConexaoMySql();
+//    JdbcTemplate con = mysql.criarConexao();
+//    List<Servidor> l1 = con.query("select * from servidor",new BeanPropertyRowMapper<>(Servidor.class));
+//   System.out.println(l1);
+        Processos processos = new Processos();
+//        System.out.println(
+//            processos.quantidadeProcessosOnline()
+//        );
+//
+//        System.out.println(
+//            processos.getAllNomesProcessos()
+//        );
+//        while(true){
+//            System.out.println(
+//                    processos.getProcessoMaiorUsoCpu()
+//            );
+            System.out.println(
+                    processos.getUsoCpuProcessos()
+            );
+//        }
 
-
+//processos.listarTodosProcessos();
 
     }
 
---
+++ API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Recurso/Processos.java
@@ -0,0 +1,67 @@
+package school.sptech.Recurso;
+
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.processos.Processo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class Processos {
+    Looca looca = new Looca();
+    public Integer quantidadeProcessosOnline(){
+        Integer qtdProcessos = 0;
+        for(Processo processo : looca.getGrupoDeProcessos().getProcessos()){
+            qtdProcessos++;
+        }
+        return qtdProcessos;
+    }
+
+    public String getProcessoMaiorUsoCpu(){
+
+        Double mediaAtualComparacao = .0;
+        Processo processoAtualComparacao;
+        List<Double> listaMediaProcessos = new ArrayList<>();
+        List<Processo> listaOrdemProcessos = new ArrayList<>();
+        for (Processo processo: looca.getGrupoDeProcessos().getProcessos()){
+            double mediaAtual = (processo.getUsoCpu() + processo.getUsoMemoria())/2;
+            listaMediaProcessos.add(mediaAtual);
+            listaOrdemProcessos.add(processo);
+        }
+
+
+        OptionalDouble maiorMediaDaLista = listaMediaProcessos.stream().mapToDouble(v -> v).max();
+        Processo processoMaiorMedia = listaOrdemProcessos.get(listaMediaProcessos.indexOf(maiorMediaDaLista.getAsDouble()));
+        return processoMaiorMedia.getNome();
+    }
+
+    public List<String> getAllNomesProcessos(){
+        List<String> listaNomes = new ArrayList<>();
+        for (Processo processo: looca.getGrupoDeProcessos().getProcessos()){
+            listaNomes.add(processo.getNome());
+        }
+        return listaNomes;
+    }
+
+    public Double getUsoCpuProcessos(){
+        Double usoTotal = .0;
+        for (Processo processo : looca.getGrupoDeProcessos().getProcessos()){
+            usoTotal += processo.getUsoCpu();
+        }
+        return usoTotal/quantidadeProcessosOnline();
+    }
+
+    public Double getUsoRamProcessos(){
+        Double usoTotal = .0;
+        for(Processo processo : looca.getGrupoDeProcessos().getProcessos()){
+            usoTotal += processo.getUsoMemoria();
+        }
+        return usoTotal;
+    }
+
+    public void listarTodosProcessos(){
+        for (Processo processo: looca.getGrupoDeProcessos().getProcessos()){
+            System.out.println(processo);
+        }
+    }
+}
