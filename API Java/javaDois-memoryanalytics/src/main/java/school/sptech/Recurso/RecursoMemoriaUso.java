61923b54251c68fb3f7abbb5c0079ce96bf38744
AdrianoAllexander <adriano.carvalho@sptech.school>
2023-12-01 04:23:14 -0300

    Java -> Semi-finalização dos métodos de recursos para CPU e RAM

--- API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Recurso/RecursoMemoriaUso.java
+++ API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Recurso/RecursoMemoriaUso.java
@@ -1,4 +1,50 @@
-package school.sptech.Recurso;
+    package school.sptech.Recurso;
+    import com.github.britooo.looca.api.core.Looca;
+    import com.github.britooo.looca.api.group.memoria.Memoria;
 
-public class RecursoMemoriaUso {
-}
+    public class RecursoMemoriaUso extends Recurso {
+
+        private final Memoria memoria;
+
+        public RecursoMemoriaUso(String nome, String unidadeMedida, Double valorRegistro) {
+            super(nome, unidadeMedida, valorRegistro);
+            this.memoria = new Memoria();
+        }
+
+        public RecursoMemoriaUso() {
+            this("Memória", "% de Uso", 0.0);
+
+            Looca looca = new Looca();
+            Memoria memoria = looca.getMemoria();
+
+            double usoDeMemoriaPercentual = calcularUsoDeMemoriaPercentual(memoria);
+
+            setValorRegistro(usoDeMemoriaPercentual);
+        }
+
+        public Memoria getMemoria() {
+            return memoria;
+        }
+
+        @Override
+        public Double capturarRegistro() {
+            return getValorRegistro();
+        }
+
+        private double calcularUsoDeMemoriaPercentual(Memoria memoria) {
+            double memoriaEmUso = memoria.getEmUso();
+            double memoriaTotal = memoria.getTotal();
+
+            if (memoriaTotal > 0) {
+                return (memoriaEmUso / memoriaTotal) * 100;
+            } else {
+                return 0.0;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Nome: %s\nUnidade de Medida: %s\nValor de Registro: %.2f\n",
+                    getNome(), getUnidadeMedida(), getValorRegistro());
+        }
+    }
--- API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Recurso/RecursoProcessadorUso.java
+++ API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Recurso/RecursoProcessadorUso.java
@@ -1,4 +1,26 @@
 package school.sptech.Recurso;
 
-public class RecursoProcessadorUso {
+import com.github.britooo.looca.api.group.processador.Processador;
+
+public class RecursoProcessadorUso extends Recurso {
+
+    private final Processador processador;
+
+    public RecursoProcessadorUso(Processador processador, String nome, String unidadeMedida) {
+        super(nome, unidadeMedida, 0.0);
+        this.processador = processador;
+    }
+
+    @Override
+    public Double capturarRegistro() {
+        double usoDoProcessador = processador.getUso();
+        setValorRegistro(usoDoProcessador);
+        return usoDoProcessador;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %s\nUnidade de Medida: %s\nValor do Registro: %.1f%%",
+                getNome(), getUnidadeMedida(), getValorRegistro());
+    }
 }
