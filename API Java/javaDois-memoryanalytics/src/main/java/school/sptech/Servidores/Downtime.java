1e76ac4f04c46033737a156f9f15bee7a6742921
danielandrade108 <daniel.andrade@sptech.school>
2023-12-01 13:52:57 -0300

    Arrumado conexão com o MySql no Java

--- API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Servidores/Downtime.java
+++ API Java/javaDois-memoryanalytics/src/main/java/school/sptech/Servidores/Downtime.java
@@ -1,20 +1,45 @@
 package school.sptech.Servidores;
 
+import com.github.britooo.looca.api.core.Looca;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import school.sptech.BancoDados.Conexao;
+import school.sptech.BancoDados.ConexaoMySql;
+import school.sptech.BancoDados.ConexaoSqlServer;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Downtime {
     private int tempoDowntime;
     private LocalDateTime dtHora;
-    private Conexao conexao;
-    private int fkServidor;
+    private List<JdbcTemplate> conexoes;
+    private Looca looca;
 
-    public Downtime(int tempoDowntime, LocalDateTime dtHora, Conexao conexao, int fkServidor) {
+    public Downtime(int tempoDowntime, LocalDateTime dtHora, int fkServidor) {
+        ConexaoSqlServer conexaoSqlServer = new ConexaoSqlServer();
+        ConexaoMySql conexaoMySql = new ConexaoMySql();
+        JdbcTemplate con1 = conexaoSqlServer.criarConexao();
+        JdbcTemplate con2 = conexaoMySql.criarConexao();
         this.tempoDowntime = tempoDowntime;
         this.dtHora = dtHora;
-        this.conexao = conexao;
-        this.fkServidor = fkServidor;
+        this.conexoes = new ArrayList<>();
+        this.looca = new Looca();
+        conexoes.add(con1);
+        conexoes.add(con2);
+    }
+
+    public int pegarId(){
+        String mac = looca.getRede().getGrupoDeInterfaces().getInterfaces().get(0).getEnderecoMac();
+
+        List<Servidor> servidors = conexoes.get(1).query("SELECT * FROM Servidor WHERE macAdress = ?",
+                new ServidorRowMapper(),
+                "192.168.1.4");
+        return servidors.get(0).getIdServidor();
+    };
+    public void calcDowntime() {
+
     }
 
     public int getTempoDowntime() {
@@ -33,29 +58,11 @@ public class Downtime {
         this.dtHora = dtHora;
     }
 
-    public Conexao getBanco() {
-        return conexao;
-    }
-
-    public void setBanco(Conexao conexao) {
-        this.conexao = conexao;
-    }
-
-    public int getFkServidor() {
-        return fkServidor;
-    }
-
-    public void setFkServidor(int fkServidor) {
-        this.fkServidor = fkServidor;
-    }
-
     @Override
     public String toString() {
         return "Downtime{" +
                 "tempoDowntime=" + tempoDowntime +
                 ", dtHora=" + dtHora +
-                ", conexao=" + conexao +
-                ", fkServidor=" + fkServidor +
                 '}';
     }
 }
