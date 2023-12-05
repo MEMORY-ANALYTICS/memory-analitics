8c6012e4743d9c221340228d915ba8d1a625e908
danielandrade108 <daniel.andrade@sptech.school>
2023-12-05 02:21:35 -0300

    Pequenas alteracoes no front da Dashboard Gerente

--- site/src/models/dashboardGModels.js
+++ site/src/models/dashboardGModels.js
@@ -16,7 +16,8 @@ function getCompProblematico(fkEmpresa) {
     "ACESSEI O FUNC MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ",
   );
   var instrucao = `
-  SELECT nomeComponente FROM getCompProblematico WHERE ${fkEmpresa};
+  SELECT nomeComponente FROM getCompProblematico WHERE fkEmpresa = ${fkEmpresa}
+  ;
     `;
   console.log("Executando a instrução SQL: \n" + instrucao);
   return database.executar(instrucao);
