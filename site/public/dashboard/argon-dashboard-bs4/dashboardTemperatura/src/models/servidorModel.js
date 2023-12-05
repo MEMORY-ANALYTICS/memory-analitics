2bc4ad8d286daebf709389612ad0e750daf44b3b
gabrielBmoraes <gabriel.moraes@sptech.school>
2023-12-05 12:35:26 -0300

    apenas linux

--- site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/models/servidorModel.js
+++ site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/models/servidorModel.js
@@ -4,7 +4,8 @@ var database = require("../database/config")
         email = 'anafonseca@email.com'
         instrucaoSql = `
         select apelidoServidor,macAdress from servidor join empresa on fkEmpresa = idEmpresa 
-        where idEmpresa = (select idEmpresa from empresa join  funcionario on fkEmpresa = idEmpresa where emailFunc = '${email}');
+        where idEmpresa = (select idEmpresa from empresa join funcionario on fkEmpresa = idEmpresa 
+          where emailFunc = '${email}' and SistemaOperacionalServidor = 'Linux');
        `
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
