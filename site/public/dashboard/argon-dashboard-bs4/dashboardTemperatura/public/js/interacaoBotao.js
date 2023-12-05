cfdb1b96bbd92a27d29ac5fd8afe48e26379ee14
gabrielBmoraes <gabriel.moraes@sptech.school>
2023-12-05 10:37:08 -0300

    dash_v7

--- site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/public/js/interacaoBotao.js
+++ site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/public/js/interacaoBotao.js
@@ -1,5 +1,7 @@
 
-function horaDash1() {
+function horaDash1(apelidoServidor) {
+
+  numeroBotao = 1
 
   idHoraDash1.style = "background-color: #5E72E4; cursor: pointer;"
   textoHora1.style = "color:  #FFFF"
@@ -9,11 +11,15 @@ function horaDash1() {
 
   idMesDash1.style = "background-color: #FFFF; cursor: pointer;"
   textoMes1.style = "color: #5e72e4;"
-
-    exibirGrafico("graficoCpuHora")
+  ctx = document.getElementById('graficoTemperatura');
+  graficoTemperatura.destroy()
+  createTemp(ctx)
+  exibirGrafico("graficoCpuHora", apelidoServidor)
 }
 
 function semanaDash1() {
+
+  numeroBotao = 2
 
   idHoraDash1.style = "background-color: #FFFF; cursor: pointer;"
   textoHora1.style = "color: #5e72e4; "
@@ -24,8 +30,9 @@ function semanaDash1() {
   idMesDash1.style = "background-color: #FFFF; cursor: pointer;"
   textoMes1.style = "color: #5e72e4; "
 
-  
-    exibirGrafico("graficoCpuSemana")
+  graficoTemperatura.destroy()
+  createTemp(ctx)
+  exibirGrafico("graficoCpuSemana")
 }
 
 function mesDash1() {
@@ -39,31 +46,49 @@ function mesDash1() {
   idMesDash1.style = "background-color: #5E72E4; cursor: pointer;"
   textoMes1.style = "color: #FFFF"
 
-  
-    exibirGrafico("graficoCpuMes")
+  graficoTemperatura.destroy()
+  createTemp(ctx)
+  exibirGrafico("graficoCpuMes")
 
 
 }
 
 
-function pesquisarIntevaloData(){
+function pesquisarIntevaloData() {
 
   var dataInicio = document.getElementById("dataInicio").value;
   var dataFim = document.getElementById("dataFim").value;
 
-  console.log(dataInicio,dataFim)
+  console.log(dataInicio, dataFim)
 
   if (dataFim < dataInicio) {
     alert("Data inválida!")
   } else {
-    
-    fetch('/grafico/filtroData').then(res=>{
+
+    fetch('/grafico/filtroData', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        dataInicioServer: dataInicio,
+        dataFimServer: dataFim,
+        fkServidor: 8
+      })
+    }).then(res => {
       res.json().then(json => {
-        for (let i=0; i < json.length; i++){
+        for (let i = 0; i < json.length; i++) {
           console.log(json[i].valor)
 
+          graficoFiltroData.data.datasets[0].data.push(json[i].valor)
+          
           var data = new Date(json[i].dia)
-          console.log(`${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`);
+          var dataCompleta = (`${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`);
+          
+          graficoFiltroData.data.labels.push(dataCompleta)
+          graficoFiltroData.update()
+
+          
         }
       })
     })
@@ -75,33 +100,44 @@ function pesquisarIntevaloData(){
 
 
 
-function exibirGrafico(tipoGrafico){
+function exibirGrafico(tipoGrafico, apelidoServidor) {
 
   console.log(tipoGrafico)
 
-  fetch(`/grafico/${tipoGrafico}`, {
+  fetch(`/grafico/${tipoGrafico}/${apelidoServidor}`, {
     method: "GET"
   }).then(res => {
     res.json().then(json => {
       for (var i = (json.length - 1); i >= 0; i--) {
-        
+
         const diaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
-        if(tipoGrafico == "graficoCpuHora"){
-            console.log(json[i].valorRegistro)
+        if (tipoGrafico == "graficoCpuHora") {
+          graficoTemperatura.data.datasets[0].data.push(json[i].valorRegistro)
+          var horario = new Date(json[i].dtHoraRegistro)
 
-        }  else if(tipoGrafico=="graficoCpuSemana"){
-          json[i].valorRegistro          
+          graficoTemperatura.data.labels.push(`${horario.getHours()}:${horario.getMinutes()}`)
+          graficoTemperatura.update()
+
+        } else if (tipoGrafico == "graficoCpuSemana") {
+
           var data = new Date(json[i].dia)
           var nomeDiaSemana = diaSemana[data.getDay()]
-          console.log(nomeDiaSemana);
+          graficoTemperatura.data.datasets[0].data.push(json[i].valorMedia)
+          graficoTemperatura.data.labels.push(nomeDiaSemana)
+          graficoTemperatura.update()
 
-        } else if(tipoGrafico == "graficoCpuMes"){
-          
+
+
+        } else if (tipoGrafico == "graficoCpuMes") {
+
           console.log(json[i].valorMedia)
-          
+
           var data = new Date(json[i].dia)
-          console.log(`${data.getDate()}/${data.getMonth()+1}`);
+          var dataDia = (`${data.getDate()}/${data.getMonth() + 1}`);
+          graficoTemperatura.data.datasets[0].data.push(json[i].valorMedia)
+          graficoTemperatura.data.labels.push(dataDia)
+          graficoTemperatura.update()
         }
       }
 
@@ -112,8 +148,8 @@ function exibirGrafico(tipoGrafico){
       console.log(err);
     })
 
-
-
-    
-
 }
+
+
+
+
--- site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/models/graficoModel.js
+++ site/public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/models/graficoModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config")
 
-function graficoCpuHora() {
+function graficoCpuHora(fkServidor) {
 
   fkServidor = 8
 
@@ -45,24 +45,42 @@ function graficoCpuMes() {
 
 function filtroData() {
 
-  var dataInicio = '2023-01-01'
-  var dataFim = '2023-12-31'
-  fkServidor = 8;
+  dataInicio = '2023-01-01'
+  dataFim = '2023-12-31'
+  fkServidor = 8
+
   
   instrucaoSql = `
   SELECT DATE(dtHoraRegistro) AS dia, round(AVG(valorRegistro),2) AS valor FROM registro
   WHERE dtHoraRegistro BETWEEN '${dataInicio}' AND '${dataFim}' and tipoMedida = "°C" 
   and fkComponente = (select idComponente from componente where fkServidor = ${fkServidor})
-  GROUP BY DATE(dtHoraRegistro);
+  GROUP BY DATE(dtHoraRegistro) order by dia;
 `
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }  
 
+function graficoIncidentes(apelidoServidor) {
+
+  
+
+  instrucaoSql = `
+  select sum(idChamadoServidor) as quantidade, Month(dtHoraChamado) as mes from 
+  chamadoServidor join componente on fkComponente = idComponente
+  join servidor on fkServidor = idServidor where descricao like "CPU" and apelidoServidor = "${apelidoServidor}"
+  group by Month(dtHoraChamado) order by mes desc;
+`
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}  
+
+
 module.exports = {
   graficoCpuHora,
   graficoCpuSemana,
   graficoCpuMes,
-  filtroData
+  filtroData,
+  graficoIncidentes
 }
