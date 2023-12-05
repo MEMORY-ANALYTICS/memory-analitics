b9c438e894d76e319e94e9822cbe2fd0e87dd320
gustavodessunte <gustavo.bernardes@sptech.school>
2023-12-05 12:38:23 -0300

    Adicionando Java Alexandra

---
+++ API Java/inserts-rede-banco-de-dados/src/main/java/school/sptech/Conexao/ConexaoSqlServer.java
@@ -0,0 +1,25 @@
+package school.sptech.Conexao;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class ConexaoSqlServer extends Conexao  {
+    private JdbcTemplate conexao;
+    public ConexaoSqlServer() {
+        super("","jdbc:sqlserver://ec2-35-172-0-77.compute-1.amazonaws.com:1433", "sa", "urubu100");
+    }
+
+
+    public JdbcTemplate criarConexao(){
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        dataSource.setURL(getUrl());
+        dataSource.setUser(getUsername());
+        dataSource.setPassword(getPassword());
+        dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
+
+        this.conexao = new JdbcTemplate(dataSource);
+        return this.conexao;
+    }
+}
+
