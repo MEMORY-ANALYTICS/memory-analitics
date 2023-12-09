2344da868a6f6ffb6fa656d46ca71ea4d36a902a
AlexandraHY14 <alexandra.yara@sptech.school>
2023-12-09 19:40:50 +0000

    Atualizando inserts SQL Server - dia 09/12

--- banco_de_dados/Scripts SQL Server/SQL_Server_Query_Create.sql
+++ banco_de_dados/Scripts SQL Server/SQL_Server_Query_Create.sql
@@ -1,12 +1,15 @@
--- IF EXISTS (SELECT * FROM sys.databases WHERE name = 'bd_memoryanalytics')
-   -- DROP DATABASE bd_memoryanalytics;
+-- DROP DATABASE IF EXISTS bd_memoryanalytics;
+--IF EXISTS (SELECT * FROM sys.databases WHERE name = 'bd_memoryanalytics')
+--DROP DATABASE bd_memoryanalytics;
 
+-- CREATE DATABASE IF NOT EXISTS bd_memoryanalytics;
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'bd_memoryanalytics')
     CREATE DATABASE bd_memoryanalytics;
 
 USE bd_memoryanalytics;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'empresa')
+-- CREATE TABLE empresa
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'empresa')
 BEGIN
     CREATE TABLE empresa(
         idEmpresa INT PRIMARY KEY IDENTITY(10000,1),
@@ -17,7 +20,8 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'endereco')
+-- CREATE TABLE endereco
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'endereco')
 BEGIN
     CREATE TABLE endereco(
         idEndereco INT PRIMARY KEY IDENTITY(1,1),
@@ -31,7 +35,8 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'cargo')
+-- CREATE TABLE cargo
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'cargo')
 BEGIN
     CREATE TABLE cargo(
         idCargo INT PRIMARY KEY IDENTITY(1,1),
@@ -39,7 +44,8 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'funcionario')
+-- CREATE TABLE funcionario
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'funcionario')
 BEGIN
     CREATE TABLE funcionario(
         idFuncionario INT PRIMARY KEY IDENTITY(100,1),
@@ -56,7 +62,8 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'login')
+-- CREATE TABLE login
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'login')
 BEGIN
     CREATE TABLE login(
         idLogin INT PRIMARY KEY IDENTITY(1,1),
@@ -67,20 +74,22 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'servidor')
+-- CREATE TABLE servidor
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'servidor')
 BEGIN
     CREATE TABLE servidor(
         idServidor INT PRIMARY KEY IDENTITY(1,1),
         SistemaOperacionalServidor VARCHAR(20),
         apelidoServidor VARCHAR(45),
-        localServidor varchar(25),
-        macAdress VARCHAR(20),
+		localServidor varchar(25),
+        macAdress varchar(25),
         fkEmpresa INT,
         FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'componente')
+-- CREATE TABLE componente
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'componente')
 BEGIN
     CREATE TABLE componente(
         idComponente INT PRIMARY KEY IDENTITY(1,1),
@@ -94,20 +103,22 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'chamadoServidor')
+
+-- CREATE TABLE chamadoServidor
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'chamadoServidor')
 BEGIN
-   CREATE TABLE chamadoServidor(
+    CREATE TABLE chamadoServidor(
         idChamadoServidor INT PRIMARY KEY IDENTITY(1,1),
         codigoChamado VARCHAR(45),
         descricao varchar(45),
         dtHoraChamado DATETIME,
-		requisitante varchar(45),
-        fkServidor INT,
-        FOREIGN KEY (fkServidor) REFERENCES servidor (idServidor)
+        fkComponente INT,
+        FOREIGN KEY (fkComponente) REFERENCES componente (idComponente)
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'downtimeServidor')
+-- CREATE TABLE downtimeServidor
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'downtimeServidor')
 BEGIN
     CREATE TABLE downtimeServidor(
         idDowntimeServidor INT PRIMARY KEY IDENTITY(1,1),
@@ -118,12 +129,14 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'registro')
+
+-- CREATE TABLE registro
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'bd_memoryanalytics' AND TABLE_NAME = 'registro')
 BEGIN
     CREATE TABLE registro(
         idRegistro INT PRIMARY KEY IDENTITY(1,1),
         valorRegistro FLOAT,
-        tipoMedida VARCHAR(25),
+		tipoMedida VARCHAR(25),
         detalheRegistro VARCHAR(45),
         dtHoraRegistro DATETIME,
         fkComponente INT,
@@ -131,26 +144,4 @@ BEGIN
     );
 END;
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'processos')
-BEGIN
-	CREATE TABLE processos(
-		idProcessos INT PRIMARY KEY IDENTITY(1,1),
-		usoCpu FLOAT,
-		usoRam FLOAT,
-		processoMaiorMediaUso VARCHAR(75),
-		qtdProcessosOnline INT,
-        dtHora DATETIME,
-		fkServidor INT,
-		FOREIGN KEY(fkServidor) REFERENCES servidor(idServidor)
-	);
-END;
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'processosBanidos')
-BEGIN
-	CREATE TABLE processosBanidos(
-		idProcesso INT PRIMARY KEY IDENTITY(1,1),
-		nomeProcesso VARCHAR(150),
-		fkServidor INT,
-		FOREIGN KEY(fkServidor) REFERENCES servidor(idServidor)
-	);
-END;
+-- Ultima atualização 09/12/2023 16:23
