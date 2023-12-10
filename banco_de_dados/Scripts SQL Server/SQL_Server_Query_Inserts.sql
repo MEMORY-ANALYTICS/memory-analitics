8919b2c1cb2a79cea9fe641b8b739e44377682e9
RaphaelKlein <raphael.almeida@sptech.school>
2023-12-10 02:35:46 +0000

    Arrumando insert rede

--- banco_de_dados/Scripts SQL Server/SQL_Server_Query_Inserts.sql
+++ banco_de_dados/Scripts SQL Server/SQL_Server_Query_Inserts.sql
@@ -138,23 +138,23 @@ INSERT INTO registro (valorRegistro, tipoMedida, detalheRegistro, dtHoraRegistro
 (10,'%','Uso RAM', '2023-10-09 10:00:00', 2),  -- RAM
 (10,'%','Uso RAM', '2023-10-09 10:00:00', 2),  -- RAM
 (10,'%','Uso RAM', '2023-10-09 10:00:00', 2),  -- RAM
-(329.415816,'MB','Enviados Rede', '2023-12-09 10:30:00', 24),  -- REDE
-(1998.433296,'MB','Recebidos Rede', '2023-12-09 10:30:00', 24),  -- REDE
-(199.632464,'MBps','Taxa de Transmissão', '2023-12-09 10:30:00', 24),  -- REDE
-(85.42277900366695, 'ms', 'Latência da Rede', '2023-12-09 10:30:00', 24), -- REDE
-(330.548499,'MB','Enviados Rede', '2023-12-09 10:30:00', 24),  -- REDE
-(2004.8182029999998,'MB','Recebidos Rede', '2023-12-09 10:30:00', 24),  -- REDE
-(201.9576005,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 24),  -- REDE
-(85.42994977827041, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 24), -- REDE
-(330.586587,'MB','Enviados Rede', '2023-12-12 10:30:00', 24),  -- REDE
-(2004.9851039999999,'MB','Recebidos Rede', '2023-12-12 10:30:00', 24),  -- REDE
-(200.079139,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 24),  -- REDE
-(200.632464,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 24),  -- REDE
-(85.43925798563782, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 24), -- REDE
-(330.548499,'MB','Enviados Rede', '2023-12-12 10:30:00', 24),  -- REDE
-(2004.8182029999998,'MB','Recebidos Rede', '2023-12-12 10:30:00', 24),  -- REDE
-(201.9576005,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 24),  -- REDE
-(85.25846318978412, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 24), -- REDE
+(329.415816,'MB','Enviados Rede', '2023-12-09 10:30:00', 35),  -- REDE
+(1998.433296,'MB','Recebidos Rede', '2023-12-09 10:30:00', 35),  -- REDE
+(199.632464,'MBps','Taxa de Transmissão', '2023-12-09 10:30:00', 35),  -- REDE
+(85.42277900366695, 'ms', 'Latência da Rede', '2023-12-09 10:30:00', 35), -- REDE
+(330.548499,'MB','Enviados Rede', '2023-12-09 10:30:00', 35),  -- REDE
+(2004.8182029999998,'MB','Recebidos Rede', '2023-12-09 10:30:00', 35),  -- REDE
+(201.9576005,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 35),  -- REDE
+(85.42994977827041, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 35), -- REDE
+(330.586587,'MB','Enviados Rede', '2023-12-12 10:30:00', 35),  -- REDE
+(2004.9851039999999,'MB','Recebidos Rede', '2023-12-12 10:30:00', 35),  -- REDE
+(200.079139,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 35),  -- REDE
+(200.632464,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 35),  -- REDE
+(85.43925798563782, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 35), -- REDE
+(330.548499,'MB','Enviados Rede', '2023-12-12 10:30:00', 35),  -- REDE
+(2004.8182029999998,'MB','Recebidos Rede', '2023-12-12 10:30:00', 35),  -- REDE
+(201.9576005,'MBps','Taxa de Transmissão', '2023-12-12 10:30:00', 35),  -- REDE
+(85.25846318978412, 'ms', 'Latência da Rede', '2023-12-12 10:30:00', 35), -- REDE
 (40, 'Mhz','Frequência CPU', '2023-10-09 10:00:00', 1),  -- CPU
 (2200, 'Mhz','Frequência CPU', '2023-10-09 10:00:00', 1),  -- CPU
 (2200, 'Mhz','Frequência CPU', '2023-10-09 10:00:00', 1),  -- CPU
