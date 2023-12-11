ed741cda91f6a640c72a4a1f898f9338ca6e4124
RaphaelKlein <raphael.almeida@sptech.school>
2023-12-11 13:06:44 -0300

    aaaaa

--- site/app.js
+++ site/app.js
@@ -1,5 +1,5 @@
- process.env.AMBIENTE_PROCESSO = "desenvolvimento";
-//process.env.AMBIENTE_PROCESSO = "producao";
+//  process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+process.env.AMBIENTE_PROCESSO = "producao";
 
 var express = require("express");
 var cors = require("cors");
@@ -22,7 +22,7 @@ var componenteRouter = require("./src/routes/componente");
 var graficoRouter = require("./public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/routes/grafico");
 var servidorRouter = require("./public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/routes/servidor");
 var kpiRouter = require("./public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/routes/kpi");
-var insertTempRouter = require("./public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/routes/insertTemp")
+// var insertTempRouter = require("./public/dashboard/argon-dashboard-bs4/dashboardTemperatura/src/routes/insertTemp")
 var dashCorrelacao = require("./src/routes/dashCorrelacao");
 var dashboardRedeRouter= require("./src/routes/dashboardRedeRoute");
 var dashboardHardwareRouter = require('./src/routes/dashboardHardware');
@@ -48,7 +48,7 @@ app.use("/dashCorrelacao",dashCorrelacao);
 app.use("/processos",processosRouter);
 app.use("/dashboardRedeRouter", dashboardRedeRouter);
 app.use("/dashboardHardware", dashboardHardwareRouter);
-app.use("/insertTemp", insertTempRouter);
+// app.use("/insertTemp", insertTempRouter);
 app.listen(PORTA, function () {
   console.log(`Servidor rodando na porta ${PORTA}`);
 });
